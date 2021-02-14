6b8d24d2d8efb45a16b5587099f3a9b1f8545433
Terence Lim Say Jian <terence.lim@go-jek.com>
2021-02-14 10:28:40 +0800

    [Terence] Add spotless rules for sql

--- src/main/resources/db/changelog/migrations/001_create_game.sql
+++ src/main/resources/db/changelog/migrations/001_create_game.sql
@@ -1,10 +1,12 @@
-create table game (
-    id varchar(255) PRIMARY KEY ,
-    game_board jsonb,
-    player_1 varchar(255),
-    player_2 varchar(255),
-    current_player varchar(255),
-    winner varchar(255),
-    created_at timestamp,
-    updated_at timestamp
-)
+CREATE
+    TABLE
+        game(
+            id VARCHAR(255) PRIMARY KEY,
+            game_board jsonb,
+            player_1 VARCHAR(255),
+            player_2 VARCHAR(255),
+            current_player VARCHAR(255),
+            winner VARCHAR(255),
+            created_at TIMESTAMP,
+            updated_at TIMESTAMP
+        )
