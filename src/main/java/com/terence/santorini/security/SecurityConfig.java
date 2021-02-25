984717a8d1f057be4537271f57c4fa534dff43fa
Terence Lim Say Jian <terence.lim@go-jek.com>
2021-02-25 10:14:30 +0800

    [Terence] Add profiles to spring security config

--- src/main/java/com/terence/santorini/security/SecurityConfig.java
+++ src/main/java/com/terence/santorini/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.terence.santorini.security;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@Profile("!local & !test")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
