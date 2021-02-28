d6a06b2ac4384158904e5e15ca2cec43cfbd1b0e
Terence Lim Say Jian <terence.lim@go-jek.com>
2021-02-28 21:18:15 +0800

    [Terence] Revert change

--- backend/src/main/java/com/terence/santorini/security/SecurityConfig.java
+++ backend/src/main/java/com/terence/santorini/security/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-@Profile("!local & !test & !prod")
+@Profile("!local & !test")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
--- backend/src/main/java/com/terence/santorini/security/ServerConfig.java
+++ backend/src/main/java/com/terence/santorini/security/ServerConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!local & !test & !prod")
+@Profile("!local & !test")
 public class ServerConfig {
 
   @Bean
